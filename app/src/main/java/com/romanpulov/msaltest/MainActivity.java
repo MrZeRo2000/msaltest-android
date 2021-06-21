d7b35aa51b6a7aba3d483a59221db65cca8b5407
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-06-21 23:30:17 +0300

    added test auth buttons

--- app/src/main/java/com/romanpulov/msaltest/MainActivity.java
+++ app/src/main/java/com/romanpulov/msaltest/MainActivity.java
@@ -4,20 +4,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import com.microsoft.identity.client.AuthenticationCallback;
 import com.microsoft.identity.client.IAccount;
+import com.microsoft.identity.client.IAuthenticationResult;
 import com.microsoft.identity.client.IPublicClientApplication;
 import com.microsoft.identity.client.ISingleAccountPublicClientApplication;
 import com.microsoft.identity.client.PublicClientApplication;
+import com.microsoft.identity.client.SilentAuthenticationCallback;
+import com.microsoft.identity.client.exception.MsalClientException;
 import com.microsoft.identity.client.exception.MsalException;
+import com.microsoft.identity.client.exception.MsalServiceException;
+import com.microsoft.identity.client.exception.MsalUiRequiredException;
 
 import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     /* Azure AD Variables */
     private ISingleAccountPublicClientApplication mSingleAccountApp;
@@ -29,6 +39,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> mSingleAccountApp.signIn((Activity)v.getContext(), null, new String[]{"user.readwrite"}, getAuthInteractiveCallback()));
+
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSingleAccountApp == null) {
+                    return;
+                }
+
+                /**
+                 * Removes the signed-in account and cached tokens from this app (or device, if the device is in shared mode).
+                 */
+                mSingleAccountApp.signOut(new ISingleAccountPublicClientApplication.SignOutCallback() {
+                    @Override
+                    public void onSignOut() {
+                        mAccount = null;
+                        Log.d(TAG, "Signed out");
+                        //updateUI();
+                        //showToastOnSignOut();
+                    }
+
+                    @Override
+                    public void onError(@NonNull MsalException exception) {
+                        Log.d(TAG, "Error signing out: " + exception.getMessage());
+                        //displayError(exception);
+                    }
+                });
+            }
+        });
+
+        Button actionButton = findViewById(R.id.actionButton);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSingleAccountApp.acquireTokenSilentAsync(new String[]{"user.readwrite"}, mAccount.getAuthority(), getAuthSilentCallback());
+            }
+        });
+
+
         // Creates a PublicClientApplication object with res/raw/auth_config_single_account.json
         PublicClientApplication.createSingleAccountPublicClientApplication(this.getApplicationContext(),
                 R.raw.auth_config_single_account,
@@ -39,12 +90,14 @@ public class MainActivity extends AppCompatActivity {
                          * This test app assumes that the app is only going to support one account.
                          * This requires "account_mode" : "SINGLE" in the config json file.
                          **/
+                        Log.d(TAG, "singleAccountApp created");
                         mSingleAccountApp = application;
                         loadAccount();
                     }
 
                     @Override
                     public void onError(MsalException exception) {
+                        Log.d(TAG, "singleAccountApp not created:" + exception.getMessage());
                         //displayError(exception);
                     }
                 });
@@ -77,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAccountLoaded(@Nullable IAccount activeAccount) {
                 // You can use the account data to update your UI or your app database.
                 mAccount = activeAccount;
+                Log.d(TAG, "AccountLoaded:" + (mAccount == null ? "null" : activeAccount.toString()));
                 //updateUI();
             }
 
@@ -91,8 +145,110 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(@NonNull MsalException exception) {
                 //displayError(exception);
+                Log.d(TAG, "AccountLoad error:" + exception.getMessage());
             }
         });
+    }
+
+    /**
+     * Callback used for interactive request.
+     * If succeeds we use the access token to call the Microsoft Graph.
+     * Does not check cache.
+     */
+    private AuthenticationCallback getAuthInteractiveCallback() {
+        return new AuthenticationCallback() {
+
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                /* Successfully got a token, use it to call a protected resource - MSGraph */
+                Log.d(TAG, "Successfully authenticated");
+                Log.d(TAG, "ID Token: " + authenticationResult.getAccount().getClaims().get("id_token"));
+
+                /* Update account */
+                mAccount = authenticationResult.getAccount();
+                //updateUI();
+
+                /* call graph */
+                //callGraphAPI(authenticationResult);
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                /* Failed to acquireToken */
+                Log.d(TAG, "Authentication failed: " + exception.toString());
+                //displayError(exception);
+
+                if (exception instanceof MsalClientException) {
+                    /* Exception inside MSAL, more info inside MsalError.java */
+                } else if (exception instanceof MsalServiceException) {
+                    /* Exception when communicating with the STS, likely config issue */
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                /* User canceled the authentication */
+                Log.d(TAG, "User cancelled login.");
+            }
+        };
+    }
+
+    /**
+     * Callback used in for silent acquireToken calls.
+     */
+    private SilentAuthenticationCallback getAuthSilentCallback() {
+        return new SilentAuthenticationCallback() {
+
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                Log.d(TAG, "Successfully authenticated with token:" + authenticationResult.getAccessToken());
+
+                /* Successfully got a token, use it to call a protected resource - MSGraph */
+                //callGraphAPI(authenticationResult);
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                /* Failed to acquireToken */
+                Log.d(TAG, "Authentication failed: " + exception.toString());
+                //displayError(exception);
+
+                if (exception instanceof MsalClientException) {
+                    /* Exception inside MSAL, more info inside MsalError.java */
+                } else if (exception instanceof MsalServiceException) {
+                    /* Exception when communicating with the STS, likely config issue */
+                } else if (exception instanceof MsalUiRequiredException) {
+                    /* Tokens expired or no session, retry with interactive */
+                }
+            }
+        };
+    }
+
+    /**
+     * Make an HTTP request to obtain MSGraph data
+     */
+    private void callGraphAPI(final IAuthenticationResult authenticationResult) {
+        Log.d(TAG, "auth token:" + authenticationResult.getAccessToken());
+        /*
+        MSGraphRequestWrapper.callGraphAPIUsingVolley(
+                getContext(),
+                graphResourceTextView.getText().toString(),
+                authenticationResult.getAccessToken(),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                          Log.d(TAG, "Response: " + response.toString());
+                        displayGraphResult(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "Error: " + error.toString());
+                        displayError(error);
+                    }
+                });
+*/
     }
 
 
